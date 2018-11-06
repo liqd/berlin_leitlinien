128f3f26c5b2593d954a3fa7d1b40cae88fd9731
Magdalena Noffke <magdalena.noffke@liqd.de>
2018-11-06 14:51:05 +0100

    disable tracking by default

--- berlin_leitlinien/settings/base.py
+++ berlin_leitlinien/settings/base.py
@@ -152,3 +152,5 @@ WAGTAILIMAGES_IMAGE_MODEL = 'images.CustomImage'
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://localhost:8000'
+
+TRACKING_ENABLED = False
