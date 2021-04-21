a7d22bb6526929a027bdced0673c830628e355e6
phillimorland <phillippamorland@gmail.com>
2021-04-21 12:29:30 +0200

    webpack.common//webpack.prod//package.json: update schema for webpack5

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,13 +3,14 @@ const common = require('./webpack.common.js')
 const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(common, {
+  devtool: 'source-map',
   optimization: {
+    minimize: true,
     minimizer: [
       new TerserPlugin({
         parallel: true,
         terserOptions: {
-          ecma: 5,
-          arrows: false
+          ecma: 5
         }
       })
     ]
